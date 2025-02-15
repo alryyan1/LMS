ef76bcf8a4a31adf7ff9f99ade2b93d5a9ef7141
alryyan <alryyan50@gmail.com>
2025-02-15 17:04:57 +0400

    s

--- src/pages/Finance/TrialBalance.tsx
+++ src/pages/Finance/TrialBalance.tsx
@@ -20,18 +20,18 @@ import AddEntryForm from "./AddEntryForm.jsx";
 import { DateField, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LoadingButton } from "@mui/lab";
-import { formatNumber } from "../constants.js";
+import { formatNumber, webUrl } from "../constants.js";
 import DateComponent from "./DateComponent.js";
 import { Account } from "../../types/type.js";
 
 function Ledger() {
   //create state variable to store all Accounts
-  const [firstDate, setFirstDate] = useState(dayjs().startOf('month'));
+  const [firstDate, setFirstDate] = useState(dayjs().startOf("month"));
   const [secondDate, setSecondDate] = useState(dayjs(new Date()));
   const [loading, setLoading] = useState(null);
   const [accounts, setAccounts] = useState<Account[]>([]);
   const [accountLedger, setAccountLedger] = useState<Account[]>([]);
-  const [selectedAccount, setSelectedAccount] = useState<Account|null>(null);
+  const [selectedAccount, setSelectedAccount] = useState<Account | null>(null);
 
   useEffect(() => {
     document.title = "دفتر الاستاذ ";
@@ -73,13 +73,21 @@ function Ledger() {
     <Grid container spacing={2}>
       <Grid item xs={8}>
         <Box sx={{ p: 1 }}>
-          <DateComponent setAccounts={setAccounts} accounts={accounts} firstDate={firstDate} secondDate={secondDate} />
+          <DateComponent
+            setAccounts={setAccounts}
+            accounts={accounts}
+            firstDate={firstDate}
+            secondDate={secondDate}
+          />
         </Box>
       </Grid>
-      <Typography variant="h3">ميزان المراجعه   </Typography>
+      <Stack alignItems={'center'} justifyContent={'center'} direction='row' gap={1}>
+      <Typography variant="h3">ميزان المراجعه </Typography>
+      <Button href={`${webUrl}trial-balance`}>PDF</Button>
+      </Stack>
 
-      <Table style={{ direction: "rtl" }} size="small">
-        <TableHead>
+      <Table style={{ direction: "rtl" }} className="table" size="small">
+        <TableHead className="thead">
           <TableRow>
             <TableCell> </TableCell>
             <TableCell> </TableCell>
@@ -98,51 +106,57 @@ function Ledger() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {accounts.map((account) => {
-            let totalCredits = account.credits.reduce(
-              (accum, current) => accum + current.amount,
-              0
-            );
-            let totalDebits = account.debits.reduce(
-              (accum, current) => accum + current.amount,
-              0
-            );
-            totalCreditSum += totalCredits;
-            totalDebitSum += totalDebits;
-            console.log(
-              totalCredits,
-              "total credits",
-              totalDebits,
-              "total dedits"
-            );
-            let largerNumber = Math.max(totalCredits, totalDebits);
-            let creditBalance = 0;
-            let debitBalance = 0;
-            if (totalCredits > totalDebits) {
-              creditBalance = totalCredits - totalDebits;
-            } else {
-              debitBalance = totalDebits - totalCredits;
-            }
-            return (
-              <TableRow
-                sx={{
-                  background: (theme) =>
-                    account.id == selectedAccount?.id
-                      ? theme.palette.warning.light
-                      : "",
-                }}
-                key={account.id}
-              >
-                <TableCell>{account.id}</TableCell>
-                <TableCell>{account.name}</TableCell>
-                <TableCell>{account.description}</TableCell>
-                <TableCell>{formatNumber(totalDebits)}</TableCell>
-                <TableCell>{formatNumber(totalCredits)}</TableCell>
-                <TableCell>{formatNumber(debitBalance)}</TableCell>
-                <TableCell>{formatNumber(creditBalance)}</TableCell>
-              </TableRow>
-            );
-          })}
+          {accounts
+            // .filter((a) => {
+
+            // return a.balance > 0
+            
+            // })
+            .map((account) => {
+              let totalCredits = account.credits.reduce(
+                (accum, current) => accum + current.amount,
+                0
+              );
+              let totalDebits = account.debits.reduce(
+                (accum, current) => accum + current.amount,
+                0
+              );
+              totalCreditSum += totalCredits;
+              totalDebitSum += totalDebits;
+              console.log(
+                totalCredits,
+                "total credits",
+                totalDebits,
+                "total dedits"
+              );
+              let largerNumber = Math.max(totalCredits, totalDebits);
+              let creditBalance = 0;
+              let debitBalance = 0;
+              if (totalCredits > totalDebits) {
+                creditBalance = totalCredits - totalDebits;
+              } else {
+                debitBalance = totalDebits - totalCredits;
+              }
+              return (
+                <TableRow
+                  sx={{
+                    background: (theme) =>
+                      account.id == selectedAccount?.id
+                        ? theme.palette.warning.light
+                        : "",
+                  }}
+                  key={account.id}
+                >
+                  <TableCell>{account.id}</TableCell>
+                  <TableCell>{account.name}</TableCell>
+                  <TableCell>{account.description}</TableCell>
+                  <TableCell>{formatNumber(totalDebits)}</TableCell>
+                  <TableCell>{formatNumber(totalCredits)}</TableCell>
+                  <TableCell>{formatNumber(debitBalance)}</TableCell>
+                  <TableCell>{formatNumber(creditBalance)}</TableCell>
+                </TableRow>
+              );
+            })}
           <TableRow>
             <TableCell> </TableCell>
             <TableCell> </TableCell>
